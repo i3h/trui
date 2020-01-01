45370d20e9cb77382dd096976b82ad31515b6ada
noobly314 <noobly314@gmail.com>
2020-01-01 18:37:23 +0800

    add component list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./header/header.component";
 
 import { CsrfInterceptorService } from "./csrf-interceptor.service";
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ListComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
