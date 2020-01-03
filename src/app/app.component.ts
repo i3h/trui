76c2afe9b7c262bbe85e13bc74b04c47b8819dc4
noobly314 <noobly314@gmail.com>
2020-01-04 00:07:15 +0800

    move functions of set torrents to main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,8 @@ export class AppComponent implements OnInit {
   isRPCBad: boolean;
   openInfo: boolean;
   shortMode: boolean;
+  focusID: string;
+  checkedAll: boolean = false;
 
   constructor(
     private router: Router,
@@ -35,6 +37,7 @@ export class AppComponent implements OnInit {
   onFocus(id: any) {
     this.openInfo = true;
     this.shortMode = true;
+    this.focusID = id;
     for (let i = 0; i < this.torrents.length; i++) {
       if (this.torrents[i].id == id) {
         this.torrent = this.torrents[i];
@@ -42,10 +45,169 @@ export class AppComponent implements OnInit {
     }
   }
 
+  onCheck(el: any) {
+    if (el.checked) {
+      this.globalService.deleteFromCheckedList(el.id);
+    } else {
+      this.globalService.addToCheckedList(el.id);
+    }
+  }
+
+  onCheckAll() {
+    console.log("main checkedall: ", this.checkedAll);
+    this.checkedAll = !this.checkedAll;
+    if (this.checkedAll) {
+      for (let i = 0; i < this.torrents.length; i++) {
+        this.torrents[i].checked = true;
+        this.globalService.addToCheckedList(this.torrents[i].id);
+      }
+    } else {
+      for (let i = 0; i < this.torrents.length; i++) {
+        this.torrents[i].checked = false;
+        this.globalService.deleteFromCheckedList(this.torrents[i].id);
+      }
+    }
+  }
+
   onCloseInfo() {
-    console.log("fired");
     this.openInfo = false;
     this.shortMode = false;
+    this.focusID = null;
+  }
+
+  translateStatusCode() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      if (this.torrents[i].status == 0) {
+        this.torrents[i].status = "Stopped";
+      } else if (this.torrents[i].status == 1) {
+        this.torrents[i].status = "Queued to check";
+      } else if (this.torrents[i].status == 2) {
+        this.torrents[i].status = "Checking";
+      } else if (this.torrents[i].status == 3) {
+        this.torrents[i].status = "Queued to download";
+      } else if (this.torrents[i].status == 4) {
+        this.torrents[i].status = "Downloading";
+      } else if (this.torrents[i].status == 5) {
+        this.torrents[i].status = "Queued to seed";
+      } else if (this.torrents[i].status == 6) {
+        this.torrents[i].status = "Seeding";
+      }
+    }
+  }
+
+  addUnit(num) {
+    if (num > 1000000000) {
+      return (num / 1000000000).toFixed(2).toString() + " GB";
+    } else if (num > 1000000) {
+      return (num / 1000000).toFixed(2).toString() + " MB";
+    } else if (num > 1000) {
+      return (num / 1000).toFixed(2).toString() + " KB";
+    } else {
+      return num.toString() + " B";
+    }
+  }
+
+  setSizeText() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      this.torrents[i].sizeText = this.addUnit(this.torrents[i].totalSize);
+    }
+  }
+
+  translateUploadedEver() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      this.torrents[i].uploadedEver = this.addUnit(
+        this.torrents[i].uploadedEver
+      );
+    }
+  }
+
+  calUploadRatio() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      this.torrents[i].uploadRatio = this.torrents[i].uploadRatio
+        .toFixed(2)
+        .toString();
+    }
+  }
+
+  setCheckedStatus() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      if (this.globalService.checkedList.includes(this.torrents[i].id)) {
+        this.torrents[i].checked = true;
+      } else {
+        this.torrents[i].checked = false;
+      }
+    }
+  }
+
+  setProgressValue() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      let value = Math.ceil(
+        (this.torrents[i].downloadedEver / this.torrents[i].totalSize) * 100
+      );
+      if (value < 100) {
+        this.torrents[i].progressValue = value;
+      } else {
+        this.torrents[i].progressValue = 100;
+      }
+    }
+  }
+
+  setProgressText() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      this.torrents[i].progressText = this.torrents[i].progressValue + "%";
+    }
+  }
+
+  setProgressColor() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      if (this.torrents[i].status == "Downloading") {
+        this.torrents[i].progressColor = "is-success";
+      } else if (this.torrents[i].status == "Seeding") {
+        this.torrents[i].progressColor = "is-info";
+      } else {
+        this.torrents[i].progressColor = "";
+      }
+    }
+  }
+
+  setStatusColor() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      if (this.torrents[i].status == "Downloading") {
+        this.torrents[i].statusColor = "green";
+      } else if (this.torrents[i].status == "Seeding") {
+        this.torrents[i].statusColor = "#23A1D8";
+      } else {
+        this.torrents[i].statusColor = "";
+      }
+    }
+  }
+
+  setRateText() {
+    for (let i = 0; i < this.torrents.length; i++) {
+      if (
+        this.torrents[i].rateDownload != 0 ||
+        this.torrents[i].rateUpload != 0
+      ) {
+        this.torrents[i].rateColor = "green";
+      }
+      this.torrents[i].rateText =
+        "D " + this.addUnit(this.torrents[i].rateDownload) + "/s";
+      this.torrents[i].rateText +=
+        " | U " + this.addUnit(this.torrents[i].rateUpload) + "/s";
+    }
+  }
+
+  refreshTorrents() {
+    this.setCheckedStatus();
+    this.translateStatusCode();
+    this.setSizeText();
+    this.translateUploadedEver();
+    this.calUploadRatio();
+    this.setProgressValue();
+    this.setProgressText();
+    this.setProgressColor();
+    this.setStatusColor();
+    this.setRateText();
   }
 
   ngOnInit() {
@@ -58,6 +220,7 @@ export class AppComponent implements OnInit {
         this.dataService.getTorrents().subscribe(res => {
           if (res != null && res.result == "success") {
             this.torrents = res.arguments.torrents;
+            this.refreshTorrents();
           }
         });
         this.dataService.getSession().subscribe(res => {
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -17,10 +17,12 @@ import { DataService } from "../data.service";
 })
 export class ListComponent implements OnInit {
   @Output() focus = new EventEmitter<any>();
+  @Output() check = new EventEmitter<any>();
+  @Output() checkAll = new EventEmitter<any>();
   @Input() torrents: any;
   @Input() shortMode: any;
-  checkedAll: boolean;
-  focusID: any;
+  @Input() focusID: string;
+  @Input() checkedAll: boolean;
 
   constructor(
     private router: Router,
@@ -30,177 +32,18 @@ export class ListComponent implements OnInit {
   ) {}
 
   onClick(el) {
-    this.focusID = el.id;
-    this.focus.emit(this.focusID);
+    //this.focusID = el.id;
+    this.focus.emit(el.id);
   }
 
-  translateStatusCode() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      if (this.torrents[i].status == 0) {
-        this.torrents[i].status = "Stopped";
-      } else if (this.torrents[i].status == 1) {
-        this.torrents[i].status = "Queued to check";
-      } else if (this.torrents[i].status == 2) {
-        this.torrents[i].status = "Checking";
-      } else if (this.torrents[i].status == 3) {
-        this.torrents[i].status = "Queued to download";
-      } else if (this.torrents[i].status == 4) {
-        this.torrents[i].status = "Downloading";
-      } else if (this.torrents[i].status == 5) {
-        this.torrents[i].status = "Queued to seed";
-      } else if (this.torrents[i].status == 6) {
-        this.torrents[i].status = "Seeding";
-      }
-    }
+  onClickCheck(el: any) {
+    this.check.emit(el);
   }
 
-  addUnit(num) {
-    if (num > 1000000000) {
-      return (num / 1000000000).toFixed(2).toString() + " GB";
-    } else if (num > 1000000) {
-      return (num / 1000000).toFixed(2).toString() + " MB";
-    } else if (num > 1000) {
-      return (num / 1000).toFixed(2).toString() + " KB";
-    } else {
-      return num.toString() + " B";
-    }
-  }
-
-  setSizeText() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      this.torrents[i].sizeText = this.addUnit(this.torrents[i].totalSize);
-    }
-  }
-
-  translateUploadedEver() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      this.torrents[i].uploadedEver = this.addUnit(
-        this.torrents[i].uploadedEver
-      );
-    }
-  }
-
-  calUploadRatio() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      this.torrents[i].uploadRatio = this.torrents[i].uploadRatio
-        .toFixed(2)
-        .toString();
-    }
-  }
-
-  setCheckedStatus() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      if (this.globalService.checkedList.includes(this.torrents[i].id)) {
-        this.torrents[i].checked = true;
-      } else {
-        this.torrents[i].checked = false;
-      }
-    }
-  }
-
-  clickCheck(el: any) {
-    if (el.checked) {
-      //el.checked = false;
-      this.globalService.deleteFromCheckedList(el.id);
-    } else {
-      //el.checked = true;
-      this.globalService.addToCheckedList(el.id);
-    }
-  }
-
-  checkAll() {
-    if (this.checkedAll) {
-      for (let i = 0; i < this.torrents.length; i++) {
-        this.torrents[i].checked = false;
-        this.globalService.deleteFromCheckedList(this.torrents[i].id);
-      }
-    } else {
-      for (let i = 0; i < this.torrents.length; i++) {
-        this.torrents[i].checked = true;
-        this.globalService.addToCheckedList(this.torrents[i].id);
-      }
-    }
-  }
-
-  setProgressValue() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      let value = Math.ceil(
-        (this.torrents[i].downloadedEver / this.torrents[i].totalSize) * 100
-      );
-      if (value < 100) {
-        this.torrents[i].progressValue = value;
-      } else {
-        this.torrents[i].progressValue = 100;
-      }
-    }
-  }
-
-  setProgressText() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      this.torrents[i].progressText = this.torrents[i].progressValue + "%";
-    }
-  }
-
-  setProgressColor() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      if (this.torrents[i].status == "Downloading") {
-        this.torrents[i].progressColor = "is-success";
-      } else if (this.torrents[i].status == "Seeding") {
-        this.torrents[i].progressColor = "is-info";
-      } else {
-        this.torrents[i].progressColor = "";
-      }
-    }
-  }
-
-  setStatusColor() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      if (this.torrents[i].status == "Downloading") {
-        this.torrents[i].statusColor = "green";
-      } else if (this.torrents[i].status == "Seeding") {
-        this.torrents[i].statusColor = "#23A1D8";
-      } else {
-        this.torrents[i].statusColor = "";
-      }
-    }
-  }
-
-  setRateText() {
-    for (let i = 0; i < this.torrents.length; i++) {
-      if (
-        this.torrents[i].rateDownload != 0 ||
-        this.torrents[i].rateUpload != 0
-      ) {
-        this.torrents[i].rateColor = "green";
-      }
-      this.torrents[i].rateText =
-        "D " + this.addUnit(this.torrents[i].rateDownload) + "/s";
-      this.torrents[i].rateText +=
-        " | U " + this.addUnit(this.torrents[i].rateUpload) + "/s";
-    }
-  }
-
-  refreshTorrents() {
-    this.setCheckedStatus();
-    this.translateStatusCode();
-    this.setSizeText();
-    this.translateUploadedEver();
-    this.calUploadRatio();
-    this.setProgressValue();
-    this.setProgressText();
-    this.setProgressColor();
-    this.setStatusColor();
-    this.setRateText();
+  onCheckAll() {
+    console.log("list checkedAll: ", this.checkedAll);
+    this.checkAll.emit();
   }
 
   ngOnInit() {}
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes["torrents"] && typeof this.torrents !== "undefined") {
-      this.refreshTorrents();
-    }
-    if (changes["shortMode"] && typeof this.shortMode !== "undefined") {
-      console.log("shortMode: ", this.shortMode);
-    }
-  }
 }
