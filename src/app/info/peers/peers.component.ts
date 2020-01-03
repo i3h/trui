272c72f6a892b775f21abe2c32f5f4142d87d881
noobly314 <noobly314@gmail.com>
2020-01-03 20:22:47 +0800

    add template to components of info

--- src/app/info/peers/peers.component.ts
+++ src/app/info/peers/peers.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  SimpleChanges
+} from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { GlobalService } from "../../global.service";
+import { DataService } from "../../data.service";
 
 @Component({
   selector: 'app-peers',
@@ -6,10 +16,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./peers.component.css']
 })
 export class PeersComponent implements OnInit {
+  @Input() torrent: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private globalService: GlobalService,
+    private dataService: DataService
+  ) {}
 
-  ngOnInit() {
+  refreshTorrent() {}
+
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["torrent"] && typeof this.torrent !== "undefined") {
+      this.refreshTorrent();
+      console.log(this.torrent);
+    }
   }
-
 }
